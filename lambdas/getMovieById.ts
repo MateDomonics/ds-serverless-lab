dad475eb170616558b429b24c7c108b13c4d7f75
Máté Domonics <domonicsmate@gmail.com>
2023-10-27 17:07:02 +0100

    Exercise: Expand get movie endpoint

--- lambdas/getMovieById.ts
+++ lambdas/getMovieById.ts
@@ -1,18 +1,17 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event, context) => { // Note change
   try {
     console.log("Event: ", event);
-    // const parameters = event?.queryStringParameters;
-    // const movieId = parameters ? parseInt(parameters.movieId) : undefined;
-    const parameters  = event?.pathParameters;
-    const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
-
+    const parameters = event?.queryStringParameters;
+    const cast = parameters?.cast == "true";
+    const movieId = event?.pathParameters?.movieId ? parseInt(event.pathParameters.movieId) : undefined;
+    
     if (!movieId) {
       return {
         statusCode: 404,
@@ -23,25 +22,51 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => { // 
       };
     }
 
-    const commandOutput = await ddbDocClient.send(
+    const commandMovie = await ddbDocClient.send(
       new GetCommand({
         TableName: process.env.TABLE_NAME,
         Key: { movieId: movieId },
       })
     );
-    console.log("GetCommand response: ", commandOutput);
-    if (!commandOutput.Item) {
+    console.log("GetCommand response: ", commandMovie);
+    if (!commandMovie.Item){
       return {
         statusCode: 404,
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify({ Message: "Invalid movie Id" }),
+        body: JSON.stringify({Message: "Invalid movie Id"}),
       };
     }
+
     const body = {
-      data: commandOutput.Item,
+      data: commandMovie.Item,
     };
+
+    if(cast) {
+      const commandCast = await ddbDocClient.send(
+        new QueryCommand({
+          TableName: process.env.CAST_TABLE_NAME, KeyConditionExpression: "movieId = :m", ExpressionAttributeValues:
+            { ":m": movieId },
+        })
+      );
+
+      //If the cast is retrived,
+      if(commandCast.Items) {
+        //The commandMovie is of the type "Record", which is simply a key and value pair from what I understand.
+        //Cast is key, and commandCast.Items is value
+        //https://stackoverflow.com/questions/66000470/add-element-to-typescript-record
+        commandMovie.Item["cast"] = commandCast.Items
+      }else{
+        return {
+          statusCode: 404,
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({ Message: "Error getting cast" }),
+        };
+      }
+    }
 
     // Return Response
     return {
